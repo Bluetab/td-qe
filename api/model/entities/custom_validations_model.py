b183c1a719b6bb07ef6833612d56460844f2a083
Nacho <ignacio.hidalgo@bluetab.net>
2018-09-26 15:46:19 +0200

    chore: refactor name implemetation key

--- api/model/entities/custom_validations_model.py
+++ api/model/entities/custom_validations_model.py
@@ -5,13 +5,13 @@ class CustomValidationsModel(BaseModel, TimestampMixin, db.Model):
 
     """Table of custom validations"""
     __tablename__ = 'custom_validations'
-    rule_implementation_id = db.Column(db.Integer)
+    implementation_key = db.Column(db.String(255))
     query_validation = db.Column(db.Text)
 
 
     @classmethod
-    def find_by_rule_implementation_id(cls, rule_implementation_id):
-        return cls.query.filter_by(rule_implementation_id=rule_implementation_id).first()
+    def find_by_implementation_key(cls, implementation_key):
+        return cls.query.filter_by(implementation_key=implementation_key).first()
 
 
     @classmethod
@@ -20,5 +20,5 @@ class CustomValidationsModel(BaseModel, TimestampMixin, db.Model):
 
 
     def to_dict(self):
-        return {'id': self.id, 'rule_implementation_id': self.rule_implementation_id,
+        return {'id': self.id, 'implementation_key': self.implementation_key,
                 'query_validation': self.query_validation}
