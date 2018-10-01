490fec21f23988f327688ce32dc5f4f40610c2f1
Nacho <ignacio.hidalgo@bluetab.net>
2018-10-01 13:27:31 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(key):
 
 setup(
     name='td_qe',
-    version='1.5.2',
+    version='2.6.0',
     description='Api for execute quality rules',
     author='bluetab',
     author_email='bluetab@bluetab.net',
